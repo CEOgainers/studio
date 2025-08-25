a0106c2408a963c70407228aebc03483a1c5cb4d
Saiful Islam <saiful.islamorpon01@gmail.com>
2025-08-25 17:31:46 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/auth/firebase.ts
+++ src/lib/auth/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps, getApp, type FirebaseApp } from 'firebase/app';
 import { getAuth, type Auth } from 'firebase/auth';
 import { getFirestore, type Firestore } from 'firebase/firestore';
+import { getAnalytics, type Analytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
@@ -9,11 +10,13 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 let app: FirebaseApp | null = null;
 let auth: Auth | null = null;
 let db: Firestore | null = null;
+let analytics: Analytics | null = null;
 
 const allConfigSet = Object.values(firebaseConfig).every(Boolean);
 
@@ -21,9 +24,12 @@ if (allConfigSet) {
     app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
     auth = getAuth(app);
     db = getFirestore(app);
+    if (typeof window !== 'undefined') {
+      analytics = getAnalytics(app);
+    }
 } else {
     console.warn("Firebase config is not fully set. Firebase will not be initialized.");
 }
 
 
-export { app, auth, db };
+export { app, auth, db, analytics };
